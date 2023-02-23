51b33e0ad2adaafe5513faa61ea76b95ccad89c7
Alison Venancio <alison.venancio@gmail.com>
2023-02-23 14:48:22 -0400

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,42 @@
+#include <Arduino.h>
+
+#define INPUT_PIN 3
+
+using namespace std;
+
+volatile uint8_t timerState = LOW;
+uint32_t task_time = 0;
+uint64_t i = 0;
+
+void pin_ISR() {
+  if(!timerState) {
+    task_time = millis();
+    Serial.println("Start Timer.");
+    timerState = HIGH;
+  } else {
+    task_time = millis() - task_time;
+    Serial.print("Stop Timer in ");
+    Serial.print(task_time);
+    Serial.println("ms.");
+    timerState = LOW;
+  }
+}
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(INPUT_PIN, INPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  attachInterrupt(digitalPinToInterrupt(INPUT_PIN), pin_ISR, RISING);
+  digitalWrite(LED_BUILTIN, LOW);
+}
+
+void loop() {
+  if(timerState)
+    digitalWrite(LED_BUILTIN, HIGH);
+  else {
+    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+    delay(200);
+  }
+
+}
